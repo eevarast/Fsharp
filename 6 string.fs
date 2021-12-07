5c587c55a1eab2b5593683c4cd195663a347623a
eevarast <93538038+eevarast@users.noreply.github.com>
2021-12-07 22:47:59 +0300

    Create 6 string.fs

---
+++ 6 string.fs
@@ -0,0 +1,16 @@
+// 17.1
+let rec pow = function
+| (s, 1) -> s
+| (s, n) -> s + pow(s, n-1)
+
+// 17.2 
+let isIthChar (s: string, n, c) = s.[n] = c
+
+// 17.3
+let rec occFromIth (s: string, n: int, c: char) =
+    let rec loop i count =
+        if i < s.Length then
+            if s.[i] = c then loop (i+1) (count+1)
+            else loop (i+1) count
+        else count
+    loop n 0
