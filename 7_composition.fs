a9b541b700c7f2f209df78c11b2314054314f376
eevarast <93538038+eevarast@users.noreply.github.com>
2021-12-12 13:24:55 +0300

    Update 7_composition.fs

--- 7_composition.fs
+++ 7_composition.fs
@@ -1,6 +1,6 @@
-let vat (n: int, x: float) = if n > 0 && n <= 100 then x * 0.01 * float(n + 100) else x
+let vat n x = if n > 0 && n <= 100 then x * float(n + 100) / 100.0 else x
 
-let unvat (n: int, x: float) = if n > 0 && n <= 100 then x / float(n + 100) * 100.0 else x
+let unvat n x = if n > 0 && n <= 100 then x / float(n + 100) * 100.0 else x
 
 let rec min f =
   let rec loop i = if f i = 0 then i else loop(i + 1)
